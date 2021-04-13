5156c19f9e83f5f6142f6e97aa657fbf5af5cf62
BoredFishRE <egb113@gmail.com>
2021-04-13 09:26:41 -0400

    yeh

---
+++ commands/about.js
@@ -0,0 +1,17 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "about",
+  description: "About the Bot",
+  aliases: ["cmvabout"],
+  execute(message, args, client) {
+    let embed = new MessageEmbed()
+      .setColor("#42f2f5")
+      .setThumbnail("https://cdn.discordapp.com/avatars/815999410327781396/7f71df8e2269680681d08e86f8c9279b.png?size=128")
+      .setFooter("CMV Clone was developed by BoredFish https://boredfish.dev", "https://cdn.discordapp.com/avatars/388813100964642816/a_f3f3f72adfde2334d6c1d2a36267a6a1.gif?size=128")
+      .addField("Bot Version", "1.2.1")
+      .addField("Chad?", "true")
+      .addField("Gamer?", "true")
+    message.channel.send(embed);
+  },
+};
---
+++ commands/hack.js
@@ -0,0 +1,12 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "hack",
+  description: "totally legit hacking tool",
+  execute(message, args, client) {
+    let embed = new MessageEmbed()
+      .setColor("#42f2f5")
+      .addField("Click this link for a tutorial on how to hack", "<https://youtu.be/rLz0dH0hF3g>")
+    message.channel.send(embed);
+  },
+};
